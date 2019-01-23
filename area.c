7f19fe87cd33ec3f8b7b2df47e1d88243727ac3c
Peter Zhao <pzhao2298@gmail.com>
2019-01-22 20:43:43 -0500

    Area of a circle

---
+++ area.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define PI 3.14
+
+int main()
+{
+int radius = 2;
+float area = PI * radius * radius;
+printf("The area of a circle with radius %d is %f\n", radius, area);
+return 0;
+}
